e16f412c609cd773313c7d4cff4c38dc40399b86
David Kreidler <david.kreidler@student.hpi.de>
2016-07-18 21:41:43 +0200

    fix request

--- bash/test.sh
+++ bash/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while read p; do
-	curl -v -H "accept:application/x-turtle" "http://localhost:8080/coal/resource?url="$p""
+	curl -v -H "accept:text/turtle" "http://localhost:8080/coal/resource?url="$p""
   done < podcasts
--- src/main/java/org/s16a/mcas/worker/id3tagWorker.java
+++
@@ -1,7 +0,0 @@
-package org.s16a.mcas.worker;
-
-/**
- * Created by davidkreidler on 13.06.16.
- */
-public class id3tagWorker {
-}
