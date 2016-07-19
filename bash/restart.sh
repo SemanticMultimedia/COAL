273844312fca7942ea0dec18e7dea57a20deaed3
boeckhoff <nico.boeckhoff@student.hpi.de>
2016-07-19 13:56:23 +0200

    reset RabbitMQ at restart. Fixes #28

--- bash/restart.sh
+++ bash/restart.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-# build and start coal server
+# Reset RabbitMQ 
+# 	RabbitMQ offers no function to flush queues, so we need to reset
+# 	and set users again
+rabbitmqctl stop_app
+rabbitmqctl reset
+rabbitmqctl start_app
+rabbitmqctl add_user coal coal
+rabbitmqctl set_user_tags coal administrator
+rabbitmqctl set_permissions -p / coal ".*" ".*" ".*"
+
+# Build and start coal server
 mvn clean install -DskipTests=true
 mvn exec:java
